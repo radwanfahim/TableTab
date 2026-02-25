02e31b2a7763e530634fd7f4b3a86da2fe75b338
radwanfahim <88280947+radwanfahim@users.noreply.github.com>
2026-02-25 23:46:25 +0600

    project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Router } from "@solidjs/router";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Route />
+      </Router>
+    </>
+  );
+}
+
+export default App;
