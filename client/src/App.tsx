4658f6ef83851d5970cbf811d28a207ef275dbe9
radwanfahim <88280947+radwanfahim@users.noreply.github.com>
2026-02-26 17:27:34 +0600

    home page added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Router } from "@solidjs/router";
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Router>
-        <Route />
+        <Route path="/" component={Home} />
       </Router>
     </>
   );
--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,5 +1,39 @@
+import Button from "../../ui/Button";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <section class="container mx-auto max-w-7xl px-4 sm:px-6">
+      {/* title */}
+      <h1
+        class="text-6xl text-(--ink) dm-serif-display-regular animate-fadeUp"
+        style="--delay: 0.1s"
+      >
+        <span>Table</span>
+        <span class="text-(--amber)">Tab</span>
+      </h1>
+      {/* submenu */}
+      <p
+        class="mt-2 text-(--text-color) dm-serif-display-regular-italic text-xl animate-fadeUp"
+        style="--delay: 0.2s"
+      >
+        Digital menus for real restaurants.
+      </p>
+
+      {/* description */}
+      <p
+        class="mt-6 text-(--text-color) jetbrains-mono text-lg animate-fadeUp"
+        style="--delay: 0.3s"
+      >
+        QR code menus, live kitchen display, and daily sales reports — for small
+        cafes and food stalls who can't afford $200/mo POS systems.
+      </p>
+
+      {/* button */}
+      <div class="animate-fadeUp" style="--delay: 0.4s">
+        <Button />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
